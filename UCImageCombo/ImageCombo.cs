6d9e36052708318a192f2db7983e510f6d855379
Zach Ayers <ayers.dev@icloud.com>
2019-06-10 09:42:52 -0500

    Add project files.

---
+++ UCImageCombo/ImageCombo.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace UCImageCombo
+{
+    public class ImageCombo : ComboBox
+    {
+        public ImageCombo()
+        {
+            DrawMode = DrawMode.OwnerDrawFixed;
+        }
+
+        public ImageList ImageList { get; set; } = new ImageList();
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+
+            if (e.Index < 0)
+
+            {
+                e.Graphics.DrawString(Text, e.Font, new SolidBrush(e.ForeColor),
+                    e.Bounds.Left + ImageList.ImageSize.Width, e.Bounds.Top);
+            }
+
+            else
+            {
+                if (Items[e.Index].GetType() == typeof(ImageComboItem))
+                {
+                    var icItem = (ImageComboItem) Items[e.Index];
+
+                    var forecolor = icItem.ForeColor != Color.FromKnownColor(KnownColor.Transparent)
+                        ? icItem.ForeColor
+                        : e.ForeColor;
+
+                    var font = icItem.Indicate ? new Font(e.Font, FontStyle.Bold) : e.Font;
+
+                    if (icItem.ImageIndex != -1)
+                    {
+                        ImageList.Draw(e.Graphics, e.Bounds.Left, e.Bounds.Top, icItem.ImageIndex);
+
+                        e.Graphics.DrawString(icItem.ItemText, font, new SolidBrush(forecolor),
+                            e.Bounds.Left + ImageList.ImageSize.Width, e.Bounds.Top);
+                    }
+                    else
+
+                    {
+                        e.Graphics.DrawString(icItem.ItemText, font, new SolidBrush(forecolor),
+                            e.Bounds.Left + ImageList.ImageSize.Width, e.Bounds.Top);
+                    }
+                }
+
+                else
+
+                {
+                    e.Graphics.DrawString(Items[e.Index].ToString(), e.Font, new SolidBrush(e.ForeColor),
+                        e.Bounds.Left + ImageList.ImageSize.Width, e.Bounds.Top);
+                }
+            }
+
+            base.OnDrawItem(e);
+        }
+    }
+}
---
+++ UCImageCombo/ImageComboItem.cs
@@ -0,0 +1,72 @@
+﻿using System.Drawing;
+
+namespace UCImageCombo
+{
+    public class ImageComboItem : object
+    {
+        public ImageComboItem()
+        {
+        }
+
+        public ImageComboItem(string Text)
+        {
+            ItemText = Text;
+        }
+
+        public ImageComboItem(string stText, int intImageIndex)
+
+        {
+            ItemText = stText;
+            ImageIndex = intImageIndex;
+        }
+
+        public ImageComboItem(string stText, int intImageIndex, bool blnIndi)
+        {
+            ItemText = stText;
+
+            ImageIndex = intImageIndex;
+
+            Indicate = blnIndi;
+        }
+
+        public ImageComboItem(string stText, int intImageIndex, bool blnIndi, Color clForeColor)
+
+        {
+            ItemText = stText;
+
+            ImageIndex = intImageIndex;
+
+            Indicate = blnIndi;
+
+            ForeColor = clForeColor;
+        }
+
+        public ImageComboItem(string stText, int intImageIndex, bool blnIndi, Color clForeColor, object oTag)
+        {
+            ItemText = stText;
+
+            ImageIndex = intImageIndex;
+
+            Indicate = blnIndi;
+
+            ForeColor = clForeColor;
+
+            Tag = oTag;
+        }
+
+        public Color ForeColor { get; set; } = Color.FromKnownColor(KnownColor.Transparent);
+
+        public int ImageIndex { get; set; } = -1;
+
+        public bool Indicate { get; set; }
+
+        public object Tag { get; set; }
+
+        public string ItemText { get; set; }
+
+        public override string ToString()
+        {
+            return ItemText;
+        }
+    }
+}
